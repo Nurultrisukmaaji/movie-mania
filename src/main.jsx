31131043a8ad8f27bd7bcde9dd6aa910830a7d5b
Nurultrisukmaaji <nurultrisukmaaji@gmail.com>
2024-12-17 11:28:13 +0700

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'; // CSS
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // JavaScript
 import 'bootstrap-icons/font/bootstrap-icons.css'; // Icons
 import './assets/style/style.css'
+import './assets/style/style_responsive.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
